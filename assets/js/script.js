2c2a8f4ebc6201eefad1cb9bda873d09089a8250
Alexandra <alexandra@datawheel.us>
2025-04-15 18:32:20 -0400

    add fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,8 @@
-// Ejercicio 2
+// Ejercicio 3
 
 function pintar(element, color = "green") {
     element.style.backgroundColor = color;
 }
 
 const ele = document.getElementById("ele1");
-ele ? ele.addEventListener("click", () => pintar(ele, "yellow")) : ""
-
-
-// 3.2
-
-console.log("Script is running");
-
-const blueSquare = document.getElementById("azul");
-const redSquare = document.getElementById("rojo");
-const greenSquare = document.getElementById("verde");
-const yellowSquare = document.getElementById("amarillo");
-
-blueSquare.addEventListener("click", () => pintar(blueSquare, "black"));
-redSquare.addEventListener("click", () => pintar(redSquare, "black"));
-greenSquare.addEventListener("click", () => pintar(greenSquare, "black"));
-yellowSquare.addEventListener("click", () => pintar(yellowSquare, "black"));
+ele.addEventListener("click", () => pintar(ele, "yellow"))
--- assets/js/script4.js
+++ assets/js/script4.js
@@ -1,3 +1,20 @@
+
+function pintar(element, color) {
+    element.style.backgroundColor = color;
+}
+
+const blueSquare = document.getElementById("azul");
+const redSquare = document.getElementById("rojo");
+const greenSquare = document.getElementById("verde");
+const yellowSquare = document.getElementById("amarillo");
+
+blueSquare.addEventListener("click", () => pintar(blueSquare, "black"));
+redSquare.addEventListener("click", () => pintar(redSquare, "black"));
+greenSquare.addEventListener("click", () => pintar(greenSquare, "black"));
+yellowSquare.addEventListener("click", () => pintar(yellowSquare, "black"));
+
+// 4.2
+
 let colorGlobal = 'white';
 
 document.getElementById('key').style.backgroundColor = colorGlobal;
